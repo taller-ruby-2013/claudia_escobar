320e5491d63cb21e5140eb5cc8a3d6da82c14eda
Claudia Escobar <cloooska@gmail.com>
2013-12-09 16:29:13 -0300

    terminada la clase 6

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,16 @@
 class CommentsController < ApplicationController
-	class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
     redirect_to post_path(@post)
   end
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
 end
 
-end
+
+
